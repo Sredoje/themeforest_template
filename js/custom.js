1bbac29d15892269eb65dc8759aa7d321c511d21
Sredoje Cutovic <sredojecutovic@gmail.com>
2013-06-30 19:58:43 +0200

    Delete

--- js/custom.js
+++ js/custom.js
@@ -9,8 +9,66 @@ $(document).ready(function(){
 		element_height= element.outerHeight();
 		resize(element_height,height);
 	});
-	
+
 	function resize(element_height , height){
 		element.css({"margin-top" : (height/2) - (element_height / 1.5) + "px"});
 	}
 });
+
+// Sticky nav
+
+$(window).load(function(){
+  $("#mainnav").sticky({ topSpacing: 0 });
+});
+
+// Smooth scroll
+
+$(document).ready(function() {
+
+    $(".scroll-links a").click(function() {
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top + "px"
+        }, {
+            duration: 500,
+            easing: "swing"
+        });
+        return false;
+    });
+
+});
+
+
+// Selectnav
+
+selectnav('fluid-nav', {
+    nested: false,
+    label: false
+});
+
+selectnav('fixed-nav', {
+    nested: false,
+    label: false
+});
+
+
+// One Page Navigation
+
+$('#fluid-nav').onePageNav({
+    currentClass: 'current',
+    changeHash: false,
+    scrollSpeed: 750,
+    scrollOffset: 30,
+    scrollThreshold: 0.5,
+    filter: ':not(.external)',
+    easing: 'swing',
+    begin: function() {
+        //I get fired when the animation is starting
+    },
+    end: function() {
+        //I get fired when the animation is ending
+    },
+    scrollChange: function($currentListItem) {
+        //I get fired when you enter a section and I pass the list item of the section
+    }
+});
+
