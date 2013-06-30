3a4f2f71b5ff7424cd03d6900e1eddbc35817b6c
Sredoje Cutovic <sredojecutovic@gmail.com>
2013-06-30 19:31:53 +0200

    Odvalili momci top marginu

--- js/center.js
+++ js/center.js
@@ -1,13 +1,14 @@
 
 
 $(document).ready(function(){
-	var window_height = window.innerHeight;
-	console.log(window_height);
-	$('#first-section-piture').css({"margin-top" : (window_height - 25) / 100 + "%"});
+		var height = $(window).outerHeight();
+		var element_height= $('#first-section-piture').outerHeight();
+		$('#first-section-piture').css({"margin-top" : (height/2) - (element_height/1.5) + "px"});
 	$(window).resize(function(){
-		var height = window.innerHeight;
-		$('#first-section-piture').css({"margin-top" : (height - 25) / 100 + "%"});
-		console.log(window_height);
+		height = $(window).outerHeight();
+		element_height= $('#first-section-piture').outerHeight();
+		$('#first-section-piture').css({"margin-top" : (height/2) - (element_height/1.5) + "px"});
+
 	});
 
 });
