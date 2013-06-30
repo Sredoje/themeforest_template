382f71d73192cea6c2fc272c908bdcc9c1d27a48
Sredoje Cutovic <sredojecutovic@gmail.com>
2013-06-30 20:11:18 +0200

    Fixed script

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,12 @@
 $(document).ready(function(){
-	var height = $(window).outerHeight();
 	var element = $('#first-section-piture');
-	var element_height = element.outerHeight();
-	resize(element_height,height);
-
 	$(window).resize(function(){
-		height = $(window).outerHeight();
-		element_height= element.outerHeight();
-		resize(element_height,height);
+	 	var height = $(window).outerHeight();
+		var element_height= element.outerHeight();
+		element.css({"margin-top" : (height/2) - (element_height / 1.5) + "px"});
 	});
 
-	function resize(element_height , height){
-		element.css({"margin-top" : (height/2) - (element_height / 1.5) + "px"});
-	}
+	$(window).resize();
 });
 
 // Sticky nav
